191ac0f13a0fe83d294b4fa4b3de818ef3dd3763
Nalzurin <alexnormandi@ukr.net>
2023-06-14 05:22:01 +0300

    Add door opening and fade to black

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 document.getElementById('knock_button').addEventListener('click', function() {
+    var button = document.getElementById('knock_button');
     var audio = document.getElementById('knocking_sound');
+    var overlay = document.getElementById('overlay');
+    var entrance = document.getElementById('entrance');
     audio.play();
+    button.parentNode.removeChild(button);
+    // Play door opening audio source after 1 second
+    setTimeout(function() {
+        audio = document.getElementById('door_open_sound');
+        audio.play();
+        setTimeout(function() {
+        overlay.style.opacity = 1;
+        overlay.style.pointerEvents = 'auto';
+        }, 1000);
+        setTimeout(function() {
+            entrance.parentNode.removeChild(entrance);
+        }, 1700);
+    }, 1500);
+
 });
